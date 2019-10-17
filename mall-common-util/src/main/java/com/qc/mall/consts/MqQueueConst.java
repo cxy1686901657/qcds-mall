f4c59b6a98c2c44f21ca0d6a16c1675e84a1ffe1
cxy1686901657 <1686901657@qq.com>
2019-10-17 20:14:20 +0800

    mq

--- mall-common-util/src/main/java/com/qc/mall/consts/MqQueueConst.java
+++ mall-common-util/src/main/java/com/qc/mall/consts/MqQueueConst.java
@@ -8,8 +8,14 @@ package com.qc.mall.consts;
  */
 public interface MqQueueConst {
 
+    //提交订单的延迟检查(支付服务)->(延迟队列)
+    String PAYMENT_CHECK_QUEUE="PAYMENT_CHECK_QUEUE";
+
     //支付完成(支付服务)->订单已支付(订单服务)
     String PAYHMENT_SUCCESS_QUEUE="PAYHMENT_SUCCESS_QUEUE";
+
+    //添加sku到搜索队列
+    String SKU_ADDTO_ES="SKU_ADDTO_ES";
 
     //订单已支付(订单服务)->库存锁定(库存系统)
     String ORDER_PAY_QUEUE="ORDER_PAY_QUEUE";
@@ -20,13 +26,11 @@ public interface MqQueueConst {
     //订单已出库(订单服务)->(订单服务)
     String ORDER_SUCCESS_QUEUE="ORDER_SUCCESS_QUEUE";
 
-    //提交订单的延迟检查(支付服务)->(延迟队列)
-    String PAYMENT_CHECK_QUEUE="PAYMENT_CHECK_QUEUE";
+
+
 
     //添加sku到缓存队列
     String SKU_ADDTO_CACHE="SKU_ADDTO_CACHE";
 
-    //添加sku到搜索队列
-    String SKU_ADDTO_ES="SKU_ADDTO_ES";
 
 }
